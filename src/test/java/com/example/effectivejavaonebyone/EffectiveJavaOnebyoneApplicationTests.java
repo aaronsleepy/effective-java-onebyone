dead49b853b8b81fe7204f563622b933de54947a
Junyi Song <junyisong@this.local>
2021-07-02 20:48:54 +0900

    First Commit

---
+++ src/test/java/com/example/effectivejavaonebyone/EffectiveJavaOnebyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivejavaonebyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaOnebyoneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
