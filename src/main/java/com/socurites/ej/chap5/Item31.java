da982d4479f3107ba974906f9404d22f8edc61f7
aaronsleepy <aaron@kmong.com>
2021-12-14 16:33:38 +0900

    Add: 31. 한정적 와일드카드를 사용해 API 유연성을 높이라

---
+++ src/main/java/com/socurites/ej/chap5/Item31.java
@@ -0,0 +1,12 @@
+package com.socurites.ej.chap5;
+
+/**
+ * 31. 한정적 와일드카드를 사용해 API 유연성을 높이라
+ * - PECS Principle: Producer Extends, Consumer Super
+ *   - Get and Put Principle
+ */
+public class Item31 {
+    public static void main(String[] args) {
+
+    }
+}
